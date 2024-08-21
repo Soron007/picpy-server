be5d6ba8f9362e88a94c358626ec5de1453bd54c
Soron007 <mitrasouvik123@gmail.com>
2024-08-21 13:14:21 +0530

    creating signup logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,31 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+const signup = async (req, res) => {
+    const { username, email, password, accountType } = req.body;
+    try {
+        let user = await User.findOne({ username: username });
+        if(user){
+            return res.status(400).json({success: false, message: "Username already in use"});
+        }
+        const securePassword = await bcrypt.hash(password, 10);
+
+        user  = new User({
+            username,
+            email,
+            password: securePassword,
+            accountType,
+        })
+    } catch (err) {
+        return res.status(500).json({ success: false, message: err.message });
+    }
+};
+
 const login = async (req, res) => {
 
 };
 
-const signup = async (req, res) => {
 
-};
 
 module.exports = { login, signup };
