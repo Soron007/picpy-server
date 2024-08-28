919d5b16472691860ec79916ee3727a5e583d7d5
Soron007 <mitrasouvik123@gmail.com>
2024-08-28 12:37:59 +0530

    changed access token duration to 20mins

--- helpers/accessToken.js
+++ helpers/accessToken.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
 const generateAccessToken = (user) =>
-  jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "15m" });
+  jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "20m" });
 
 module.exports = { generateAccessToken };
