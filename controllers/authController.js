6a04ff25ba2021f5cdb89708a0f45f3742b34229
Souvik <mitrasouvik123@gmail.com>
2024-09-28 10:35:48 +0530

    wrote profile change api

--- controllers/authController.js
+++ controllers/authController.js
@@ -67,47 +67,90 @@ const login = async (req, res) => {
   }
 };
 
-const refreshToken = async(req, res) => {
+const refreshToken = async (req, res) => {
   const refreshTokenForThisRequest = req.headers.authorization?.split(" ")[1];
 
-  if(!refreshTokenForThisRequest) {
-    return res.status(401).json({success: false, message: "Unauthorized, Please login!"});
+  if (!refreshTokenForThisRequest) {
+    return res
+      .status(401)
+      .json({ success: false, message: "Unauthorized, Please login!" });
   }
 
   try {
-    jwt.verify(refreshTokenForThisRequest, process.env.REFRESH_TOKEN_SECRET, (err, user)=> {
-    if(err) return res.status(403).json({success: false, message: err.message})
+    jwt.verify(
+      refreshTokenForThisRequest,
+      process.env.REFRESH_TOKEN_SECRET,
+      (err, user) => {
+        if (err)
+          return res.status(403).json({ success: false, message: err.message });
 
-      const accessToken = generateAccessToken({
-        id: user.id,
-        message: "new access token",
-        accountType: user.accountType,
-        author: user.author,
-      });
+        const accessToken = generateAccessToken({
+          id: user.id,
+          message: "new access token",
+          accountType: user.accountType,
+          author: user.author,
+        });
 
-      const refreshToken = generateRefreshToken({
-        id: user.id,
-        message: "new refresh token",
-        accountType: user.accountType,
-        author: user.author,
+        const refreshToken = generateRefreshToken({
+          id: user.id,
+          message: "new refresh token",
+          accountType: user.accountType,
+          author: user.author,
+        });
 
-      });
-
-      return res.status(200).json({
-        success: true,
-        message: "Token refreshed successfully",
-        accessToken,
-        refreshToken, 
-        role: user.accountType,
-        author: user.author,
-      });
-    });     
-    
+        return res.status(200).json({
+          success: true,
+          message: "Token refreshed successfully",
+          accessToken,
+          refreshToken,
+          role: user.accountType,
+          author: user.author,
+        });
+      }
+    );
   } catch (error) {
     console.log(error);
-    res.status(403).json({success: false, message: "Invalid refresh token"});
+    res.status(403).json({ success: false, message: "Invalid refresh token" });
   }
-}
+};
 
+const switchProfile = async (req, res) => {
+  const authorId = req.id;
+  const authorAccountType = req.accountType;
 
-module.exports = { login, signup, refreshToken };
+  try {
+    const user = await User.findByIdAndUpdate(authorId, {
+      accountType: authorAccountType === "buyer" ? "seller" : "buyer",
+    });
+
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "user not found" });
+    }
+
+    const data = {
+      id: user._id,
+      accountType: user.accountType,
+      author: user.username,
+    };
+
+    const accessToken = generateAccessToken(data);
+    const refreshToken = generateRefreshToken(data);
+
+    return res
+      .status(200)
+      .json({
+        success: true,
+        message: `Switched to ${user.accountType}`,
+        accessToken,
+        refreshToken,
+        role: user.accountType,
+        author: user.username,
+      });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+module.exports = { login, signup, refreshToken, switchProfile};
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,14 +1,13 @@
-// Ye hai aam zindagi
-// const express = require("express")
-// const router = express.Router()
 
-const { login, signup, refreshToken } = require("../controllers/authController");
 
-// Ye hai mentos zindagi
+const { login, signup, refreshToken, switchProfile } = require("../controllers/authController");
+const verifyToken = require('../middleware/verifyToken')
+
 const router = require("express").Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.get("/refresh", refreshToken);
+router.get("/switch", verifyToken, switchProfile);
 
 module.exports = router;
