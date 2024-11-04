407a625127312d5d2bdce90291fbbaa90b611518
Souvik <mitrasouvik123@gmail.com>
2024-11-04 19:55:24 +0530

    completed prototype

--- controllers/postController.js
+++ controllers/postController.js
@@ -14,15 +14,9 @@ const createPost = async (req, res) => {
   const { title, author, price, image, publicId } = req.body;
 
   try {
-    const post = new Post({
-      title,
-      author,
-      price,
-      image,
-      publicId,
-      authorId,
-    });
+    const post = new Post({ title, author, price, image, publicId, authorId });
     await post.save();
+
     await User.findByIdAndUpdate(authorId, {
       $push: { uploads: post._id },
     });
@@ -38,12 +32,10 @@ const createPost = async (req, res) => {
 const getAllPosts = async (req, res) => {
   try {
     const posts = await Post.find({});
-
-    if (posts.length === 0) {
+    if (posts.length === 0)
       return res
         .status(404)
         .json({ success: false, message: "No posts found" });
-    }
 
     return res.status(200).json({ success: true, data: posts });
   } catch (error) {
@@ -54,29 +46,22 @@ const getAllPosts = async (req, res) => {
 const getMyPosts = async (req, res) => {
   const authorId = req.id;
   const authorAccountType = req.accountType;
-
   try {
-    // Find the user by ID and populate the purchased posts
-
-    if (authorAccountType === "Buyer") {
+    if (authorAccountType === "buyer") {
       const { purchased } = await User.findById(authorId).populate("purchased");
       console.log(purchased);
-      if (!purchased) {
+      if (!purchased)
         return res
           .status(404)
           .json({ success: false, message: "No posts found" });
-      }
 
       return res.status(200).json({ success: true, data: purchased });
     } else {
       const { uploads } = await User.findById(authorId).populate("uploads");
-
-      if (!uploads) {
+      if (!uploads)
         return res
           .status(404)
-          .json({ success: false, message: "No uploads found" });
-      }
-
+          .json({ success: false, message: "No posts found" });
       return res.status(200).json({ success: true, data: uploads });
     }
   } catch (error) {
@@ -88,60 +73,49 @@ const getMyPosts = async (req, res) => {
 
 const deletePost = async (req, res) => {
   const { id } = req.params;
-
   try {
-    const post = await post.findById(id);
-
-    if (!post) {
+    const post = await Post.findById(id);
+    if (!post)
       return res
         .status(404)
-        .json({ success: false, message: "post not found" });
-    }
+        .json({ success: false, message: "Post not found " });
 
     const { authorId } = post;
-
     await User.findByIdAndUpdate(authorId, {
       $pull: { uploads: id },
     });
-    // //we will not do this as some of the people had already purchased your asset
+
+    // we will not do this as some of the people had already purchased your asset
     // await Post.findByIdAndDelete(id);
 
     return res
       .status(200)
-      .json({ success: true, message: "post deleted successfully" });
+      .json({ success: true, message: "Post deleted successfully" });
   } catch (error) {
-    console.log(error);
     return res.status(500).json({ success: false, message: error.message });
   }
 };
 
-const searchPost = async (req, res) => {
+const searchPosts = async (req, res) => {
   const { search } = req.query;
-
   try {
     const posts = await Post.find({ title: { $regex: search, $options: "i" } });
     if (posts.length == 0)
-      return res
-        .status(404)
-        .json({ success: false, message: "no posts found" });
+      return res.status(404).json({ success: false, message: "No post found" });
 
-    return res.status(200).json({ success: true, data: posts });
+    return res.status(200).json({ success: false, data: posts });
   } catch (error) {
-    console.log(error);
     return res.status(500).json({ success: false, message: error.message });
   }
 };
 
 const addToFavourites = async (req, res) => {
   const { authorId } = req.id;
-
   const { postId } = req.params;
-
   try {
     const user = await User.findByIdAndUpdate(authorId, {
       $push: { favourites: postId },
     });
-
     if (!user)
       return res
         .status(404)
@@ -151,21 +125,16 @@ const addToFavourites = async (req, res) => {
       .status(200)
       .json({ success: true, message: "Post added to favourites" });
   } catch (error) {
-    console.log(error);
     return res.status(500).json({ success: false, message: error.message });
   }
 };
-
 const removeFromFavourites = async (req, res) => {
   const { authorId } = req.id;
-
   const { postId } = req.params;
-
   try {
     const user = await User.findByIdAndUpdate(authorId, {
       $pull: { favourites: postId },
     });
-
     if (!user)
       return res
         .status(404)
@@ -173,27 +142,21 @@ const removeFromFavourites = async (req, res) => {
 
     return res
       .status(200)
-      .json({ success: true, message: "Post removed to favourites" });
+      .json({ success: true, message: "Post removed from favourites" });
   } catch (error) {
-    console.log(error);
     return res.status(500).json({ success: false, message: error.message });
   }
 };
-
 const getFavourites = async (req, res) => {
   const authorId = req.id;
-
   try {
     const { favourites } = await User.findById(authorId).populate("favourites");
-
     if (!favourites)
       return res
         .status(404)
-        .json({ success: false, message: "No favourites found" });
-
+        .json({ success: false, message: "No favourites added" });
     return res.status(200).json({ success: true, data: favourites });
   } catch (error) {
-    console.log(error);
     return res.status(500).json({ success: false, message: error.message });
   }
 };
@@ -201,32 +164,28 @@ const getFavourites = async (req, res) => {
 const getPostsByDateRange = async (req, res) => {
   const authorId = req.id;
   const authorAccountType = req.accountType;
-
   let data;
 
   try {
-    if (authorAccountType === "buyer") {
+    if (authorAccountType == "buyer") {
       const { purchased } = await User.findById(authorId).populate("purchased");
-      console.log(purchased);
       data = purchased;
     } else {
       const { uploads } = await User.findById(authorId).populate("uploads");
-      console.log(uploads);
       data = uploads;
     }
 
-    if (!data) {
-      return res.status(500).json({ success: false, message: "Nothing found" });
-    }
+    console.log(data)
+
+    if (!data)
+      return res
+        .status(500)
+        .json({ success: false, message: "No posts found" });
 
     const now = new Date();
-    const startOfWeek = new Date(now.setDate(now.getDate() - now.getDay()));
-
     const startOfYear = new Date(now.getFullYear(), 0, 1);
-
     const startOfMonth = new Date(now.getFullYear(), now.getMonth(), 1);
-
-    
+    const startOfWeek = new Date(now.setDate(now.getDate() - now.getDay()));
 
     const postsThisYear = data.filter(
       (post) => new Date(post.createdAt) >= startOfYear
@@ -238,10 +197,6 @@ const getPostsByDateRange = async (req, res) => {
       (post) => new Date(post.createdAt) >= startOfWeek
     );
 
-    console.log("Posts this week:", postsThisWeek);
-    console.log("Posts this month:", postsThisMonth);
-    console.log("Posts this year:", postsThisYear);
-
     return res.status(200).json({
       success: true,
       data: {
@@ -252,7 +207,6 @@ const getPostsByDateRange = async (req, res) => {
       },
     });
   } catch (error) {
-    console.log(error);
     return res.status(500).json({ success: false, message: error.message });
   }
 };
@@ -262,7 +216,7 @@ module.exports = {
   getAllPosts,
   getMyPosts,
   deletePost,
-  searchPost,
+  searchPosts,
   addToFavourites,
   removeFromFavourites,
   getFavourites,
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,30 +1,29 @@
+const router = require("express").Router();
+const verifyToken = require("../middleware/verifyToken")
 const {
   createPost,
-  getAllPosts,
   getMyPosts,
+  getAllPosts,
   deletePost,
-  searchPost,
+  searchPosts,
   addToFavourites,
   removeFromFavourites,
   getFavourites,
   getPostsByDateRange,
 } = require("../controllers/postController");
-const verifyToken = require("../middleware/verifyToken");
-
-const router = require("express").Router();
 
 router.post("/post/create", verifyToken, createPost);
 router.get("/post/getAll", getAllPosts);
 router.get("/post/myPosts", verifyToken, getMyPosts);
 router.delete("/post/delete/:id", verifyToken, deletePost);
-router.get("/post/search", searchPost);
-router.post("/post/addToFavourites/:postId", verifyToken, addToFavourites);
-router.post(
+router.get("/post/search", searchPosts);
+router.put("/post/addToFavourites/:postId", verifyToken, addToFavourites);
+router.put(
   "/post/removeFromFavourites/:postId",
   verifyToken,
   removeFromFavourites
 );
 router.get("/post/favourites", verifyToken, getFavourites);
-router.get("/post/getPostsByDateRange", verifyToken, getPostsByDateRange)
+router.get("/post/getPostsByDateRange", verifyToken, getPostsByDateRange);
 
 module.exports = router;
